7ac26f37f617bc426888b66a3d8fdb3d026c4e3a
ItsNotAI LABS <MedinaSITECH@outlook.com>
2026-07-17 18:50:27 -0500

    inject wallet vault and Office capabilities

--- auro_native_llm/production_fleet/__init__.py
+++ auro_native_llm/production_fleet/__init__.py
@@ -4,5 +4,8 @@ from .runtime import AgentManager, ModelEndpoint, NovaRuntime
 from .organ_sdk import AuroOrganSDK, SDKConfig
 from .capabilities import NativeCapabilities
 from .receipts import ReceiptLedger
+from .wallet import PaperWallet
+from .office import NativeOffice
+from .vault import IntegrityVault
 
-__all__ = ["AgentManager", "ModelEndpoint", "NovaRuntime", "AuroOrganSDK", "SDKConfig", "NativeCapabilities", "ReceiptLedger"]
+__all__ = ["AgentManager", "ModelEndpoint", "NovaRuntime", "AuroOrganSDK", "SDKConfig", "NativeCapabilities", "ReceiptLedger", "PaperWallet", "NativeOffice", "IntegrityVault"]
